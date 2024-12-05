e3947876e3fae0eebac1743bb3c60a98a8da999e
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-12-05 18:22:53 +0500

    Categoryni create qilish va drag qilib orderini o'zgartirish qo'shildi

--- app/Livewire/CategoryComponent.php
+++ app/Livewire/CategoryComponent.php
@@ -10,7 +10,15 @@ class CategoryComponent extends Component
     // View variables
     public $categories = [];
 
+    //Variables for Creation
+    public $createForm = true, $name;
+
     public function mount()
+    {
+        $this->categories();
+    }
+
+    public function categories()
     {
         $this->categories = Category::orderBy('order','asc')->get();
     }
@@ -22,6 +30,23 @@ class CategoryComponent extends Component
 
     public function updateCategoryOrder($categoryIds)
     {
-        dd($categoryIds);
+        foreach ($categoryIds as $categoryId) {
+            Category::where('id',$categoryId['value'])->update(['order' => $categoryId['order']]);
+        }
+        $this->categories();
+    }
+
+    public function create()
+    {
+        $this->createForm = !$this->createForm;
+    }
+
+    public function store()
+    {
+        Category::create([
+            'name' => $this->name,
+        ]);
+        $this->reset(['createForm','name']);
+        $this->categories();
     }
 }
--- resources/views/livewire/category-component.blade.php
+++ resources/views/livewire/category-component.blade.php
@@ -1,5 +1,14 @@
 <div>
     <h2><strong>Categories</strong></h2>
+    <button class="btn btn-primary mt-2 mb-3" wire:click="create">{{ $createForm ? 'Create' : 'Back' }}</button>
+    @if (!$createForm)
+    <form wire:submit.prevent="store">
+        <div class="mb-3">
+            <input type="text" wire:model="name" class="form-control" placeholder="Category name">
+            <button type="submit" class="btn btn-success mt-3">Create</button>
+        </div>
+    </form>
+    @endif
     <table class="table table-striped table-bordered table-hover">
         <thead>
             <tr>
