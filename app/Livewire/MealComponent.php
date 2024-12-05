1e88e0a0c94365b89d7c9b63315f7dc2bac97fb3
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-12-05 15:34:57 +0500

    Category bilan Meal Model,Componentlari yaratildi va Migrationlar

---
+++ app/Livewire/MealComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MealComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.meal-component');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Livewire\CategoryComponent;
+use App\Livewire\MealComponent;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/',CategoryComponent::class);
+Route::get('/meal',MealComponent::class);
