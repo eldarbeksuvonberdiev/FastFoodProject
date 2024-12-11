b2e9defce4e0c7d5d81604399e45c43c4b2e242b
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-12-11 10:37:45 +0500

    Adminda order'ni checkbox orqali statusini o'zgartirish qo'shildi

--- resources/views/livewire/order-component.blade.php
+++ resources/views/livewire/order-component.blade.php
@@ -15,49 +15,68 @@
                 <div class="card card-row card-secondary">
                     <div class="card-header">
                         <h3 class="card-title">
-                            Backlog
+                            All Orders
                         </h3>
                     </div>
                     <div class="card-body">
-                        <div class="card card-info card-outline">
-                            <div class="card-header">
-                                <h5 class="card-title">Create Labels</h5>
-                                <div class="card-tools">
-                                    <a href="#" class="btn btn-tool btn-link">#3</a>
-                                    <a href="#" class="btn btn-tool">
-                                        <i class="fas fa-pen"></i>
-                                    </a>
+                        @forelse ($givenOrders as $givenOrder)
+                            <div class="card card-info card-outline">
+                                <div class="card-header">
+                                    <h5 class="card-title">Order {{ $givenOrder->id }} {!! $givenOrder->status == '3' ? "<span class='text-danger'>Ready</span>" : '' !!}</h5>
+                                    <div class="card-tools">
+                                        <a class="btn btn-info" data-bs-toggle="collapse"
+                                            href="#info{{ $givenOrder->id }}" role="button" aria-expanded="false"
+                                            aria-controls="multiCollapseExample1"><i class="bi bi-pencil"></i></a>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="collapse multi-collapse" id="info{{ $givenOrder->id }}">
+                                            <div class="card card-body">
+                                                @if ($givenOrder->orderItems->count() > 0)
+                                                    <div class="form-check">
+                                                        <input class="form-check-input"
+                                                            wire:click="orderReady({{ $givenOrder->id }})"
+                                                            type="checkbox" value="" id="orderItem"
+                                                            {{ $givenOrder->status == '3' ? 'checked disabled' : '' }}>
+                                                        <label class="form-check-label" for="orderItem">
+                                                            Order Ready
+                                                        </label>
+                                                    </div>
+                                                @endif
+                                                @forelse ($givenOrder->orderItems as $orderItem)
+                                                    <div class="form-check">
+                                                        <input class="form-check-input"
+                                                            wire:click="changeStatus({{ $givenOrder }},{{ $orderItem->id }}, '3')"
+                                                            type="checkbox" value=""
+                                                            id="orderItem{{ $orderItem->id }}"
+                                                            {{ $orderItem->status == '3' ? 'checked disabled' : '' }}>
+                                                        <label class="form-check-label"
+                                                            for="orderItem{{ $orderItem->id }}">
+                                                            {{ $orderItem->meal->name . ' x ' . $orderItem->count }}
+                                                        </label>
+                                                    </div>
+                                                @empty
+                                                    This order has no items
+                                                @endforelse
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="card-body">
-                                <div class="custom-control custom-checkbox">
-                                    <input class="custom-control-input" type="checkbox" id="customCheckbox1" disabled>
-                                    <label for="customCheckbox1" class="custom-control-label">Bug</label>
-                                </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input class="custom-control-input" type="checkbox" id="customCheckbox2" disabled>
-                                    <label for="customCheckbox2" class="custom-control-label">Feature</label>
-                                </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input class="custom-control-input" type="checkbox" id="customCheckbox3" disabled>
-                                    <label for="customCheckbox3" class="custom-control-label">Enhancement</label>
-                                </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input class="custom-control-input" type="checkbox" id="customCheckbox4" disabled>
-                                    <label for="customCheckbox4" class="custom-control-label">Documentation</label>
-                                </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input class="custom-control-input" type="checkbox" id="customCheckbox5" disabled>
-                                    <label for="customCheckbox5" class="custom-control-label">Examples</label>
+                        @empty
+                            <div class="card card-info card-outline">
+                                <div class="card-header">
+                                    <h5 class="card-title">No Orders are here yet</h5>
                                 </div>
                             </div>
-                        </div>
+                        @endforelse
                     </div>
                 </div>
                 <div class="card card-row card-primary">
                     <div class="card-header">
                         <h3 class="card-title">
-                            To Do
+                            In Progress
                         </h3>
                     </div>
                     <div class="card-body">
@@ -77,7 +96,7 @@
                 <div class="card card-row card-default">
                     <div class="card-header bg-info">
                         <h3 class="card-title">
-                            In Progress
+                            Done
                         </h3>
                     </div>
                     <div class="card-body">
@@ -105,7 +124,7 @@
                 <div class="card card-row card-success">
                     <div class="card-header">
                         <h3 class="card-title">
-                            Done
+                            Catered
                         </h3>
                     </div>
                     <div class="card-body">
