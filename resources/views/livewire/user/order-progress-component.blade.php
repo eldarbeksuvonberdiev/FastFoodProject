fc0251624554d9d42307adc2b345b7c68314f3e9
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-12-16 16:35:24 +0500

    Ozgina o'zgarishlar

--- resources/views/livewire/user/order-progress-component.blade.php
+++ resources/views/livewire/user/order-progress-component.blade.php
@@ -2,7 +2,7 @@
     <div class="container text-center">
         <div class="row align-items-center">
             <div class="col-4">
-                <div class="card-header">
+                <div class="card-header text-light" style="background-color: grey; ">
                     Received Orders
                 </div>
                 <div class="card-body">
@@ -16,30 +16,30 @@
                 </div>
             </div>
             <div class="col-4">
-                <div class="card-header">
+                <div class="card-header text-light" style="background-color: blue;">
                     In progress
                 </div>
                 <div class="card-body">
                     @forelse ($inProgresses as $inProgress)
-                        <span class="text-secondar">
+                        <span class="text-primary">
                             {{ $inProgress->queue }}
                         </span>
                     @empty
-                        <span class="text-secondary">No orders</span>
+                        <span class="text-primary">No orders</span>
                     @endforelse
                 </div>
             </div>
             <div class="col-4">
-                <div class="card-header">
+                <div class="card-header text-light" style="background-color: green; ">
                     Ready to take
                 </div>
                 <div class="card-body">
                     @forelse ($readies as $ready)
-                        <span class="text-secondar">
+                        <span class="text-success">
                             {{ $ready->queue }}
                         </span>
                     @empty
-                        <span class="text-secondary">No orders</span>
+                        <span class="text-success">No orders</span>
                     @endforelse
                 </div>
             </div>
